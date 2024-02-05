7cbf0a7a46d6d7e4d68a204257ec2745a5e07530
RaulMurillo <ramurimontero@gmail.com>
2024-02-05 19:16:26 +0100

    Removed old synthesis data with incorrect design and 45nm

--- rtl/myDCscript.sh
+++ rtl/myDCscript.sh
@@ -2,25 +2,20 @@
 
 echo "My dc_shell script"
 
+source ~/scripts/synopsys_setup.sh
+
 C_DIR=$PWD
 for d in *_bit*
 do
 	cd ${d}/syn
-	for f in 500MHz 1GHz 
-	do
-		cd $f
-		echo ""
-		echo ""
-		echo "$PWD"
-		echo 
-		echo ""
-		echo ""
-		if [ -f "run.scr" ]; then
-			rm -rf reports
-			mkdir reports
-			dc_shell-xg-t -f run.scr
-		fi
-		cd ..
-	done
+	echo ""
+	echo ""
+	echo "$PWD"
+	echo 
+	echo ""
+	echo ""
+	if [ -f "dc_synthesis.tcl" ]; then
+		dc_shell-xg-t -f dc_synthesis.tcl
+	fi
 	cd $C_DIR
 done
